ab6708b274af24a3ece72e67aae928046b39729f
José Robert <jose.goncalves@conductor.com.br>
2019-11-21 13:16:20 -0300

    mapping attribute with JPA e add attribute phone

--- src/main/java/com/spring/course/domain/User.java
+++ src/main/java/com/spring/course/domain/User.java
@@ -3,6 +3,10 @@ package com.spring.course.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 import com.spring.course.domain.enums.Role;
 
 import lombok.AllArgsConstructor;
@@ -14,12 +18,24 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
+@Entity(name = "user")
 public class User {
 	
+	@Id
 	private Long id;
+	
+	@Column(length = 75, nullable = false)
 	private String name;
+	
+	@Column(length = 75, nullable = false, unique = true)
 	private String email;
+	
+	@Column(length = 100, nullable = false)
 	private String password;
+	
+	@Column(length = 20)
+	private String phone;
+	
 	private Role role;
 	private List<Request> requests = new ArrayList<Request>();
 	private List<RequestStage> stages =  new ArrayList<RequestStage>();
