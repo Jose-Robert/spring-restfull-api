e9db8c8bdfd57738e044400aed6abe3cdcb0e0f4
José Robert <robertspfc10@gmail.com>
2019-11-22 01:02:20 -0300

    map entities

--- src/main/java/com/spring/course/domain/Request.java
+++ src/main/java/com/spring/course/domain/Request.java
@@ -1,6 +1,21 @@
 package com.spring.course.domain;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import com.spring.course.domain.enums.RequestState;
 
@@ -14,14 +29,33 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@SuppressWarnings("all")
+@Entity(name = "request")
 public class Request {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(length = 75, nullable = false)
 	private String subject;
+	
+	@Column(columnDefinition = "text")
 	private String description;
+	
+	@Column(name = "creation_date", nullable = false)
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date creationDate;
-	private RequestStage requestStage;
+	
+	@OneToMany(mappedBy = "request")
+	private List<RequestStage> stage = new ArrayList<RequestStage>();
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id", nullable = false)
 	private User user;
-	private RequestState states;
+	
+	@Column(length = 12, nullable = false)
+	@Enumerated(EnumType.STRING)
+	private RequestState state;
 	
 }
--- src/main/java/com/spring/course/domain/RequestStage.java
+++ src/main/java/com/spring/course/domain/RequestStage.java
@@ -2,6 +2,18 @@ package com.spring.course.domain;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 import com.spring.course.domain.enums.RequestState;
 
 import lombok.AllArgsConstructor;
@@ -14,12 +26,30 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@SuppressWarnings("all")
+@Entity(name = "request_stage")
 public class RequestStage {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name = "realization_date", nullable = false)
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date realizationDate;
+	
+	@Column(columnDefinition = "text")
 	private String description;
+	
+	@ManyToOne
+	@JoinColumn(name = "request_id", nullable = false)
 	private Request request;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id", nullable = false)
 	private User user;
+	
+	@Column(length = 12, nullable = false)
+	@Enumerated(EnumType.STRING)
 	private RequestState state;
 }
